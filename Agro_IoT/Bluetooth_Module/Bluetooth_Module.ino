de595ce5a04da08e272589cdce70143a0f4ec6b5
Giovanni <99917909+giovannirdias@users.noreply.github.com>
2022-07-31 19:18:22 -0300

    V0 - Necessário escrever o código para esse módulo específico

--- Agro_IoT/Bluetooth_Module/Bluetooth_Module.ino
+++ Agro_IoT/Bluetooth_Module/Bluetooth_Module.ino
@@ -1,3 +1,9 @@
+/*
+ * Códido para módulo bluetooth que enviará os dados adquiridos pelos sensores diretamente ao dashboard
+ * Placas: ESP8266
+ * COM: Geralmente COM6 e COM7
+ */
+
 void setup() {
 
 }
